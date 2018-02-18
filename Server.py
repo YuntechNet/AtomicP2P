ac385010747eb52828b041e02fa35a97ca149dba
clooooode <jackey8616@gmail.com>
2018-02-18 12:16:20 +0800

    make server to a object instance

--- Server.py
+++ Server.py
@@ -1,6 +1,43 @@
 from Config import Config
 import sys, socket
 
+
+class LibServer:
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print('Socket created on host: %s' % HOST)
+
+        try:
+            self.sock.bind((self.host, self.port))
+        except socket.error as err:
+            print('Bind failed. Error Code : ' .format(err))
+
+        self.sock.listen(10)
+        print("Socket Listening on port %d" % self.port)
+
+        self.stopSig = False
+
+    def loop(self):
+        try:
+            conn, addr = self.sock.accept()
+            conn.send(bytes("Message"+"\r\n",'UTF-8'))
+            print("Message sent")
+            data = conn.recv(1024)
+            print(data.decode(encoding='UTF-8'))
+        except socket.error as e:
+            self.sock.close()
+            print(e)
+        return self.loop() if self.stopSig == False else None
+
+    def start(self):
+        self.loop()
+
+    def stop(self):
+        self.stopSig = True
+
 HOST = Config.SERVER_HOST
 PORT = Config.SERVER_PORT
 
@@ -10,25 +47,5 @@ for each in sys.argv:
     elif '--PORT=' in each:
         PORT = int(each[7:])
 
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Socket created on host: %s' % HOST)
-
-try:
-    sock.bind((HOST, PORT))
-except socket.error as err:
-    print('Bind failed. Error Code : ' .format(err))
-
-sock.listen(10)
-print("Socket Listening on port %d" % PORT)
-
-while(True):
-    try:
-        conn, addr = sock.accept()
-        conn.send(bytes("Message"+"\r\n",'UTF-8'))
-        print("Message sent")
-        data = conn.recv(1024)
-        print(data.decode(encoding='UTF-8'))
-    except socket.error as e:
-        sock.close()
-        print(e)
+libServer = LibServer(HOST, PORT)
+libServer.loop()
