919424a45d623ecc05838c183dbcf7f838017dba
clode <jackey8616@gmail.com>
2018-05-21 22:09:17 +0800

    Remove useless file and add run.py

--- Server.py
+++ Server.py
@@ -24,19 +24,3 @@ class Server(threading.Thread):
                 LinkTo = Client(addr[0],7000, 'checklink')
                 LinkTo.start()
 
-if __name__ == '__main__' :
-    IP_dictionary = {'MyIP':'0.0.0.0', 'switch maneger':'192.168.43.53'}
-    s = Server('0.0.0.0',8000)
-    s.start()
-
-    while True:
-        cmd = input('imput what do you want to do:')
-        if cmd=='help':
-            print('S: send your message to aim.')
-        elif cmd == 'S':
-            ip = str(input ('what the host do you want to send:'))
-            pt = int(input ('what the port do you use:'))
-            cmd = input('imput is your command:')
-            print('I send to',ip,pt)
-            Sendcmd = Client(ip, pt, cmd)
-            Sendcmd.start()
---
+++ run.py
@@ -0,0 +1,19 @@
+from Server import Server
+from Client import Client
+
+if __name__ == '__main__' :
+    IP_dictionary = {'MyIP':'0.0.0.0', 'switch maneger':'192.168.43.53'}
+    s = Server('0.0.0.0',8000)
+    s.start()
+
+    while True:
+        cmd = input('imput what do you want to do:')
+        if cmd=='help':
+            print('S: send your message to aim.')
+        elif cmd == 'S':
+            ip = str(input ('what the host do you want to send:'))
+            pt = int(input ('what the port do you use:'))
+            cmd = input('imput is your command:')
+            print('I send to',ip,pt)
+            Sendcmd = Client(ip, pt, cmd)
+            Sendcmd.start()
