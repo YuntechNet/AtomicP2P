292cdf4fe6da985cd170240d5f17693c30734fa1
clooooode <jackey8616@gmail.com>
2018-11-01 23:19:19 +0800

    Fix test not pass due to sendCommand renamed

--- tests/tests_2_integrat/device/test_integrat_ssh.py
+++ tests/tests_2_integrat/device/test_integrat_ssh.py
@@ -1,22 +1,7 @@
 
 
-def test_sendCommand(ssh):
-    ssh.login()
-    assert ssh.is_active() is True
-    ssh.sendCommand('\x08')
-    ssh.sendCommand('\x0A')
-    ssh.sendCommand('\x0A')
-    ssh.sendCommand('exit')
-    ssh.logout()
-    assert ssh.is_active() is False
-
-
 def test_send_command(ssh):
     ssh.login()
     assert ssh.is_active() is True
-    ssh.send_command('\x08')
-    ssh.send_command('\x0A')
-    ssh.send_command('\x0A')
-    ssh.send_command('exit')
     ssh.logout()
     assert ssh.is_active() is False
