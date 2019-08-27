2429bdc984446a17b1a87d2203bc091bde4d0489
clooooode <jackey8616@gmail.com>
2019-08-28 01:36:02 +0800

    Add integrate test for join_net_by_DNS method

--- tests/tests_2_integrat/peer/command/test_joincmd.py
+++ tests/tests_2_integrat/peer/command/test_joincmd.py
@@ -1,9 +1,11 @@
 from time import sleep
 
 
-def test_join_net(core1, switch1):
+def test_join_with_host(core1, switch1):
     assert len(core1._Peer__send_queue) == 0
-    core1.join_net(host=switch1.server_info.host)
+    host = "{}:{}".format(switch1.server_info.host[0],
+                          switch1.server_info.host[1])
+    core1._on_command(["join", host])
     assert len(core1._Peer__send_queue) == 1
     sleep(5)
     assert switch1.server_info in core1.connectlist
@@ -12,7 +14,7 @@ def test_join_net(core1, switch1):
 
 def test_join_net_by_DNS(core1, switch1):
     assert len(core1._Peer__send_queue) == 0
-    core1.join_net_by_DNS(domain="atomic_p2p.integrattest.org")
+    core1._on_command(["join", "atomic_p2p.integrattest.org"])
     assert len(core1._Peer__send_queue) == 1
     sleep(5)
     assert switch1.server_info in core1.connectlist
